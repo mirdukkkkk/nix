841c87696de6eb939f82e72af45cf4876e479538
mirdukkkkk <karmulav@gmail.com>
2026-09-06 14:12:51 +1200

    impermanence => preservation

--- modules/nixos/core/nixpkgs.nix
+++ modules/nixos/core/nixpkgs.nix
@@ -3,9 +3,6 @@
     nixpkgs = {
         config = {
             allowUnfree = true;
-            permittedInsecurePackages = [
-                "pnpm-10.29.2"
-            ];
         };
         overlays = [
             inputs.claude-code.overlays.default
